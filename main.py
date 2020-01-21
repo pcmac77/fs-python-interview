312dc36f6d6708fd8fd60debb0fc11b8298fca53
Peter Chuang <pchuang@a10networks.com>
2020-01-21 17:04:24 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# Please implement the python programs imported in the following statements.
+# Time Limit: 75 minutes
+###########
+# 1. Fibonacci
+print('[1] ' + '#' * 12)
+import fibonacci
+
+###########
+# 2. Reverse Words
+print('[2] ' + '#' * 12)
+import reverse_words
+
+###########
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# NOTE:  Please skip this one!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 3. Simple Transposition
+# print('[3] ' + '#' * 12)
+# import simple_transposition
+
+###########
+# 4. File Downloader
+print('[4] ' + '#' * 12)
+import file_downloader
+
+###########
+# 5. Longest Palindrome
+print('[5] ' + '#' * 12)
+import longest_palindrome
